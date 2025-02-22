138bdc8ca60f1d0b9e1d30aec4a31a96ac8ef177
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-02-22 10:38:49 +0100

    feat: update CORS configuration to specify allowed origins for development

--- app.ts
+++ app.ts
@@ -8,19 +8,15 @@ import "./src/utils/passport-utils.js";
 const app = express();
 app.use(express.json());
 
-// Middleware dla formularzy (w tym `multipart/form-data`)
 app.use(express.urlencoded({ extended: true }));
 
-// Konfiguracja `multer` (folder `uploads/` dla plików)
-// const upload = multer({ dest: "uploads/" });
-
 const corseOptions = {
-  // origin: [
-  //   "http://example.com",
-  //   "http://localhost:3000",
-  //   "http://localhost:5000",
-  // ],
-  origin: true, // for dev
+  origin: [
+    "http://example.com",
+    "http://localhost:3000",
+    "http://localhost:5000",
+  ],
+  // origin: true, // for dev
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
